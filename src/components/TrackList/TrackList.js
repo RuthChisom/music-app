657ccc93e5c641a3a00dda77ab6dc85ebd0fdca2
Ruth Chisom <ndukwe100805043@gmail.com>
2022-06-09 12:09:48 +0100

    Fix errors

--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -3,12 +3,21 @@ import "./TrackList.css";
 import Track from "../Track/Track";
 
 class TrackList extends React.Component{
+    constructor(props){
+        super(props);
+    
+        this.state = {
+          tracks : []
+        };
+    }
+
     render(){
         return(
             <div className="TrackList">
-                {this.props.tracks.map(track=>{
+                {/* {this.state.props.map(track=>{ */}
+                {this.state.tracks.map(track=>{
                     return(
-                        <track track={track} key={track.id} onAdd={this.props.onAdd} isRemoval={this.props.isRemoval} onRemove={this.props.onRemove} />
+                        <Track track={track} key={track.id} onAdd={this.props.onAdd} isRemoval={this.props.isRemoval} onRemove={this.props.onRemove} />
                     )
                 })}
             </div>
